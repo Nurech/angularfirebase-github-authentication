863bad944e041014601b34f2206a4134799a4212
Joosep Parts <joosep.parts@gmail.com>
2022-05-28 07:00:43 +0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCNOg5DVxLpiKWSj-EbIE5EDLg1XUxBFJU",
+    authDomain: "github-authentication-75a59.firebaseapp.com",
+    projectId: "github-authentication-75a59",
+    storageBucket: "github-authentication-75a59.appspot.com",
+    messagingSenderId: "890130377781",
+    appId: "1:890130377781:web:d075a9925498be9af5157c",
+    measurementId: "G-EN2G66WCM3"
+  },
 };
