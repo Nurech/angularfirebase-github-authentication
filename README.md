9548431e783bc1e51651d82e1ab7987fa651d0c7
Joosep Parts <joosep.parts@gmail.com>
2022-05-28 06:22:20 +0300

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Angular 13 Authentication with Firebase and GitHub
+
+Learn to create full Firebase authentication system with Angular using AngularFire2 library.
+
+## Written Article
+[Adding login/signup authentication to your web application using GitHub GraphQL and Firebase](https://medium.com/p/8a0f192458f8)
+
+
+- ⭐ Sign in with GitHub
+- Sign in with Google
+- Sign in with username/password
+- Sign up with username/password
+- Recover forget password
+- Send verification mail to a newly created user
+- Prevent user to access app dashboard unless email is verified
+- Prevent user to access inner pages unless user is not logged In
+- Prevent user to access sign in and sign up components when a user is already logged in
+- Save user data to localstorage when user is logged in
+
+
+
+
+## Installation Process
+Run `npm install` to install all the required dependencies
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. 
+
+The app will automatically reload if you change any of the source files.
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCNOg5DVxLpiKWSj-EbIE5EDLg1XUxBFJU",
+    authDomain: "github-authentication-75a59.firebaseapp.com",
+    projectId: "github-authentication-75a59",
+    storageBucket: "github-authentication-75a59.appspot.com",
+    messagingSenderId: "890130377781",
+    appId: "1:890130377781:web:d075a9925498be9af5157c",
+    measurementId: "G-EN2G66WCM3"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
